9f35448c411ec3d0c8b9defcf380296a4d32375c
iamdhikka <suryamiharja96@gmail.com>
2025-07-15 14:29:59 +0700

    ardhieeeeeeeee

--- vitest.config.js
+++ vitest.config.js
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // <-- tambahkan ini
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src') // <-- tambahkan ini
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   test: {
     globals: true,
     environment: 'jsdom',
     coverage: {
       reporter: ['text', 'lcov'],
-      reportsDirectory: 'coverage'
-    }
-  }
+      reportsDirectory: 'coverage',
+      all: true,
+      include: ['src/**/*.{js,ts,vue}'],
+    },
+  },
 })
