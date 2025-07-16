bc36a88c2860d164cc9e91025521c68ee59f3c09
locitakaes42 <locitakaes@gmail.com>
2025-07-16 17:07:13 +0700

    fix

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -13,13 +13,13 @@ export const useAuthStore = defineStore('auth', () => {
 
   // --- Firebase Client SDK initialization ---
   const firebaseConfig = {
-    apiKey: "AIzaSyDexM23IlaX_UGOnVu-XqGCWm2MsTYhzIo",
-    authDomain: "p3am-165d0.firebaseapp.com",
-    projectId: "p3am-165d0",
-    storageBucket: "p3am-165d0.firebasestorage.app",
-    messagingSenderId: "259915858128",
-    appId: "1:259915858128:web:8e1cad616206101cdcded5",
-    measurementId: "G-VKY3VP8KKE"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
   };
   const firebaseApp = initializeApp(firebaseConfig);
   const firebaseAuth = getAuth(firebaseApp);
