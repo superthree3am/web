362d8d4aa0863bd45910e6f69d252a9cc0f6b13a
iamdhikka <suryamiharja96@gmail.com>
2025-07-17 16:56:25 +0700

    8

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,12 @@ module.exports = {
   },
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ecmaVersion: 2020,
     sourceType: 'module',
   },
   extends: [
-    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential', // kamu pakai ini di package.json
     'eslint:recommended'
   ],
   rules: {
