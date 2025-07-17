ac358db461722c7938b26cbc1c6e0526d12aeccb
iamdhikka <suryamiharja96@gmail.com>
2025-07-17 17:01:42 +0700

    9

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,22 @@ module.exports = {
     sourceType: 'module',
   },
   extends: [
-    'plugin:vue/vue3-essential', // kamu pakai ini di package.json
-    'eslint:recommended'
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
   ],
   rules: {
     'no-unused-vars': 'warn',
-    'vue/multi-word-component-names': 'off'
-  }
+    'vue/multi-word-component-names': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.js'],
+      env: {
+        jest: true, // ⬅️ Ini akan aktifkan globals: describe, it, expect
+      },
+      rules: {
+        'no-undef': 'off', // ⬅️ Matikan error "not defined" karena sudah dihandle jest
+      },
+    },
+  ],
 };
