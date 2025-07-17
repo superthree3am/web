65afc22574a676f1dd8cadec913b3e129666243c
iamdhikka <suryamiharja96@gmail.com>
2025-07-17 16:22:40 +0700

    5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,33 @@
 module.exports = {
-  // ...
-  // plugins: ['vitest'], // HAPUS ini
+  root: true,
   env: {
-    // 'vitest/globals': true, // HAPUS ini
-    jest: true, // GANTI dengan ini jika perlu
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true, // jika kamu pakai vitest, juga aman
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'vue/multi-word-component-names': 'off', // jika kamu pakai nama file "index.vue"
   },
   overrides: [
     {
       files: ['**/*.spec.js'],
-      env: {
-        jest: true, // ini aktifkan global seperti `describe`, `it`, `expect`
-      },
+      env: { jest: true },
+      rules: { 'no-undef': 'off' },
     },
   ],
 };
